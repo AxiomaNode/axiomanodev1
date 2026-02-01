319d68cb38fb4e19ca460e957d93f5ca7a1f842e
alendeMTR <alende073@gmail.com>
2026-02-01 20:52:09 +0500

    importes fixed again

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global.css";
-import App from "./App/App";
+import App from "./App/App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
