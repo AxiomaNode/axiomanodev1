e383d9a49c44848fe9372f0dfb3ce4045de63ed2
alendeMTR <alende073@gmail.com>
2026-02-20 21:35:08 +0500

    another test

--- api/support.js
+++ api/support.js
@@ -1,5 +1,10 @@
-return res.status(200).json({
-  hasUser: !!process.env.EMAIL_USER,
-  hasPass: !!process.env.EMAIL_PASS,
-  hasTo: !!process.env.EMAIL_TO,
-});
+export default async function handler(req, res) {
+  return res.status(200).json({
+    ok: true,
+    marker: "SUPPORT_API_V1",
+    method: req.method,
+    hasUser: !!process.env.EMAIL_USER,
+    hasPass: !!process.env.EMAIL_PASS,
+    hasTo: !!process.env.EMAIL_TO,
+  });
+}
