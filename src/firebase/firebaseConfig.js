c270654e649791fe37c2bd0311ca5fedeff102fc
alendeMTR <alende073@gmail.com>
2026-03-25 20:37:09 +0500

    check completed

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -15,5 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
